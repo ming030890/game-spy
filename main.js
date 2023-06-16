a5bd6a64ea4dd3da942b3a137213aebc72f2b9c2
Tony Mak <tony@Tonys-MacBook-Air.local>
2023-06-16 16:36:54 +0800

    v2

--- main.js
+++ main.js
@@ -7,6 +7,10 @@ const io = require("socket.io")(httpServer, {
   },
 });
 
+let alivePlayers = [];
+let spyUserName;
+let gameover = true;
+
 async function emitNumUsers() {
   const sockets = await io.fetchSockets();
   const numPlayers = sockets.length;
@@ -15,29 +19,78 @@ async function emitNumUsers() {
 
 function onNewChallenge(socket) {
   socket.on('new_challenge', async () => {
+    alivePlayers = [];
+    gameover = false;
     const sockets = await io.fetchSockets();
     const numPlayers = sockets.length;
     const challenge = challenges[Math.floor(Math.random() * challenges.length)];
     const spyChallengeId = Math.floor(Math.random() * 2);
     const spy = Math.floor(Math.random() * numPlayers);
-    console.log('sockets.entries(): ' + sockets.length);
     for (const [index, socket] of sockets.entries()) {
-      if (index == spy) {
-        socket.emit('challenge', challenge[spyChallengeId]);
+      alivePlayers.push(socket.username);
+    }
+    for (const [index, socket] of sockets.entries()) {
+      const response = {};
+      response['players'] = alivePlayers;
+      if (index == spy) { 
+        response['challenge'] = challenge[spyChallengeId];
+        spyUserName = socket.username;
       } else {
-        socket.emit('challenge', challenge[1 - spyChallengeId]);
+        response['challenge'] = challenge[1 - spyChallengeId];
       }
+      socket.emit('challenge', response);
     }
   });
 }
 
+function onVote(socket) {
+  socket.on('vote', async (username) => {
+    if (gameover) {
+      return;
+    }
+    const index = alivePlayers.indexOf(username);
+    if (index < 0) {
+      return;
+    }
+    alivePlayers.splice(index, 1);
+    io.emit('next_round', alivePlayers);
+    if (username === spyUserName) {
+      const response = {};
+      response['winner'] = 'citizens';
+      response['spy'] = spyUserName;
+      io.emit('win', response);
+      gameover = true;
+      return;
+    }
+    if (alivePlayers.length <= 2) {
+      const response = {};
+      response['winner'] = 'spy';
+      response['spy'] = spyUserName;
+      io.emit('win', response);
+      gameover = true;
+      return;
+    }
+  });
+}
+
+io.use((socket, next) => {
+  const username = socket.handshake.auth.username;
+  if (!username) {
+    return next(new Error("invalid username"));
+  }
+  socket.username = username;
+  next();
+});
+
 io.on('connection', async (socket) => {
-  console.log(`a user connected ${socket.id}`);
+  console.log(`a user connected ${socket.username}`);
   socket.on('disconnect', () => {
-    console.log(`user disconnected ${socket.id}`);
+    console.log(`user disconnected ${socket.username}`);
+    emitNumUsers();
   });
   await emitNumUsers();
   onNewChallenge(socket);
+  onVote(socket);
 });
 
 const port = process.env.PORT || 3000;
